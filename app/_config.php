a0a5f8cad9b20bd55adb3c3fd5e78d2c445a52c7
Kartik Patel <kartikpatel@outlook.co.nz>
2019-10-11 14:43:25 +1300

    updates

--- app/_config.php
+++ app/_config.php
@@ -1,2 +1,12 @@
 <?php
  
+use SilverStripe\Forms\HTMLEditor\HTMLEditorConfig;
+
+HTMLEditorConfig::get('cms')->enablePlugins(['textcolor', 'colorpicker']);
+HtmlEditorConfig::get('cms')->insertButtonsBefore('formatselect', 'forecolor');
+HtmlEditorConfig::get('cms')->insertButtonsBefore('formatselect', 'backcolor');
+HtmlEditorConfig::get('cms')->insertButtonsBefore('formatselect', 'fontsizeselect');
+
+HtmlEditorConfig::get('cms')->setOptions([
+    'fontsize_formats' => '8pt 10pt 11pt 12pt 14pt 18pt 24pt 36pt'
+]);
