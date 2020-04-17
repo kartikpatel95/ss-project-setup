8ae29db6355ccd96b61b4ab12530625a723f0587
Kartik Patel <kartikpatel@outlook.co.nz>
2020-04-17 21:45:15 +1200

    Add Social icons gridfield

--- app/src/Extensions/SiteConfig_Extensions.php
+++ app/src/Extensions/SiteConfig_Extensions.php
@@ -1,16 +1,63 @@
+<?php
+
+namespace App\Extensions;
+
 use App\Models\SocialMedia;
+use SilverStripe\AssetAdmin\Forms\UploadField;
+use SilverStripe\Assets\Image;
+use SilverStripe\Forms\FieldList;
+use SilverStripe\Forms\GridField\GridField;
+use SilverStripe\Forms\GridField\GridFieldConfig_RecordEditor;
+use SilverStripe\ORM\DataExtension;
+use Symbiote\GridFieldExtensions\GridFieldOrderableRows;
+
+/**
+ * Class SiteConfig_Extensions
+ *
+ * @package App\Extensions
+ */
+class SiteConfig_Extensions extends DataExtension
+{
+
+    /**
+     * @var string[]
+     */
+    private static $has_one = [
+        'Logo' => Image::class,
+    ];
+
+
     private static $has_many = [
         'SocialIcons' => SocialMedia::class,
     ];
+
+    /**
+     * @var string[]
+     */
+    private static $owns = [
+        'Logo'
+    ];
+
+
+    public function updateCMSFields(FieldList $fields)
+    {
+        $fields->addFieldToTab('Root.Main', $logo = UploadField::create('Logo'));
+        $logo->setFolderName('SiteLogo');
+        $logo->getValidator()->setAllowedExtensions(['jpg', 'jpeg', 'gif', 'png']);
+
         $fields->addFieldToTab('Root.Social', $this->getSocialGrid());
 
+    }
 
-    public function getSocialGrid()
+    /**
+     * @return GridField
+     */
+    public function getSocialGrid(): GridField
     {
         $grid = GridField::create('SocialIcons', 'Social Media', $this->owner->SocialIcons(),
             GridFieldConfig_RecordEditor::create());
 
-//        $grid->getConfig()->addComponent(new GridFieldOrderableRows('SortOrder'));
+        $grid->getConfig()->addComponent(new GridFieldOrderableRows('SortOrder'));
 
         return $grid;
     }
---
+++ app/src/Models/SocialMedia.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Models;
+
+use gorriecoe\Link\Models\Link;
+use gorriecoe\LinkField\LinkField;
+use SilverStripe\Forms\TextField;
+use SilverStripe\ORM\DataObject;
+use SilverStripe\SiteConfig\SiteConfig;
+
+/**
+ * Class SocialMedia
+ *
+ * @package App\Models
+ */
+class SocialMedia extends Link
+{
+
+    /**
+     * @var string
+     */
+    private static $table_name = "SocialMedia";
+
+    /**
+     * @var string
+     */
+    private static $singular_name = "Social Media";
+
+    /**
+     * @var string
+     */
+    private static $plural_name = "Social Media";
+
+    /**
+     * @var string[]
+     */
+    private static $db = [
+        'Icon' => 'Varchar',
+        'SortOrder' => 'Int'
+    ];
+
+    /**
+     * @var string[]
+     */
+    private static $has_one = [
+        'SiteConfig' => SiteConfig::class,
+    ];
+
+    /**
+     * @var string[]
+     */
+    private static $summary_fields = [
+        'Icon',
+        'Social',
+    ];
+
+
+    public function getCMSFields()
+    {
+        $fields = parent::getCMSFields();
+        $fields->removeFieldsFromTab('Root.Main', ['SiteConfigID', 'SortOrder', 'SocialID']);
+
+        $fields->addFieldToTab('Root.Main', TextField::create('Icon')
+            ->setDescription('Get your social icons from the following URL <a href="https://fontawesome.com/">Font Awesome</a>'),
+            'OpenInNewWindow');
+
+
+        return $fields;
+    }
+}
